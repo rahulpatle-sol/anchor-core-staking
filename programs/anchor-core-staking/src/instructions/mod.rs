a63d975a958d9993115323993b118dbd643ed925
Andreia Canadas <36857252+AndreiaCanadas@users.noreply.github.com>
2026-06-02 13:48:23 +0100

    builders q2 staking version

---
+++ programs/anchor-core-staking/src/instructions/mod.rs
@@ -0,0 +1,12 @@
+pub mod initialize;
+pub mod stake;
+pub mod unstake;
+pub mod create_collection;
+pub mod mint_asset;
+// pub mod claim_rewards;
+
+pub use initialize::*;
+pub use stake::*;
+pub use unstake::*;
+pub use create_collection::*;
+pub use mint_asset::*;
